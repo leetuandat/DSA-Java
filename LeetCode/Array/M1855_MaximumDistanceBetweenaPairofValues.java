8c73174a87c438b4ac0c99af69545b8bb0c0fe3c
leetuandat <tuandat24034@gmail.com>
2026-04-20 23:40:25 +0700

    update 1 solution for M1855

--- LeetCode/Array/M1855_MaximumDistanceBetweenaPairofValues.java
+++ LeetCode/Array/M1855_MaximumDistanceBetweenaPairofValues.java
@@ -61,5 +61,29 @@ public class M1855_MaximumDistanceBetweenaPairofValues {
         return maxDis;
     }
 
+    /**
+     * Binary Search
+     */
+    public int maxDistance3(int[] nums1, int[] nums2) {
+        int maxDist = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            int left = i;
+            int right = nums2.length;
+            int bestJ = -1;
+            while (left < right) {
+                int mid = left + (right - left)/2;
+                if (nums2[mid] >= nums1[i]) {
+                    bestJ = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            if (bestJ != -1) {
+                maxDist = Math.max(maxDist, bestJ - i);
+            }
+        }
+        return maxDist;
+    }
     
 }
