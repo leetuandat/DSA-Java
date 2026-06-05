8bde7946c455ebbca600c84b2494b9f80a0840dc
leetuandat <tuandat24034@gmail.com>
2026-06-05 22:40:50 +0700

    keep streak

---
+++ LeetCode/Array/E3633_EarliestFinishTimeForLandAndWaterRidesI.java
@@ -0,0 +1,15 @@
+/**
+ * The link of this problem is <a href = "https://leetcode.com/problems/earliest-finish-time-for-land-and-water-rides-i"> here </a>.
+ *
+ * @author X.e.n.g
+ * @version 1.0
+ * @project name: LeetCode
+ * @date: 05-Jun-26
+ * @time: 10:40 PM
+ * @package: LeetCode.Array
+ */
+
+package LeetCode.Array;
+
+public class E3633_EarliestFinishTimeForLandAndWaterRidesI {
+}
