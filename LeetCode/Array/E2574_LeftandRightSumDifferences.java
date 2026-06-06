0180b3e4a8de0022a9b8a8671d5a6fb6d18a29a0
leetuandat <tuandat24034@gmail.com>
2026-06-06 19:20:27 +0700

    keep streak

---
+++ LeetCode/Array/E2574_LeftandRightSumDifferences.java
@@ -0,0 +1,15 @@
+/**
+ * The link of this problem is <a href = "https://leetcode.com/problems/left-and-right-sum-differences"> here </a>.
+ *
+ * @author X.e.n.g
+ * @version 1.0
+ * @project name: LeetCode
+ * @date: 06-Jun-26
+ * @time: 07:19 PM
+ * @package: LeetCode.Array
+ */
+
+package LeetCode.Array;
+
+public class E2574_LeftandRightSumDifferences {
+}
