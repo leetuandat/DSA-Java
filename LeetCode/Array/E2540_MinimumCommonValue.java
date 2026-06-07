76fd235b785e701f1963327c5a7d6b9b3de26cf0
leetuandat <tuandat24034@gmail.com>
2026-06-07 23:28:11 +0700

    keep streak

---
+++ LeetCode/Array/E2540_MinimumCommonValue.java
@@ -0,0 +1,15 @@
+/**
+ * The link of this problem is <a href = "https://leetcode.com/problems/minimum-common-value"> here </a>.
+ *
+ * @author X.e.n.g
+ * @version 1.0
+ * @project name: LeetCode
+ * @date: 07-Jun-26
+ * @time: 11:27 PM
+ * @package: LeetCode.Array
+ */
+
+package LeetCode.Array;
+
+public class E2540_MinimumCommonValue {
+}
