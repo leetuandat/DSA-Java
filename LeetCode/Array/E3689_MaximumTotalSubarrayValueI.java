3cb21d078bdea25bf36cbeffe4e071fca686f7bc
leetuandat <tuandat24034@gmail.com>
2026-06-09 23:31:40 +0700

    keep streak

---
+++ LeetCode/Array/E3689_MaximumTotalSubarrayValueI.java
@@ -0,0 +1,15 @@
+/**
+ * The link of this problem is <a href = "https://leetcode.com/problems/maximum-total-subarray-value-i"> here </a>.
+ *
+ * @author X.e.n.g
+ * @version 1.0
+ * @project name: LeetCode
+ * @date: 09-Jun-26
+ * @time: 11:30 PM
+ * @package: LeetCode.Array
+ */
+
+package LeetCode.Array;
+
+public class E3689_MaximumTotalSubarrayValueI {
+}
